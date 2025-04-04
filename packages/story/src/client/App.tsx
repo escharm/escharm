dadf20bdd16afbcb57296107bb8d143543ff5e4b
cqh963852 <cqh963852@hotmail.com>
2025-04-04 11:04:46 +0800

    chore: update demo

--- packages/story/src/client/App.tsx
+++ packages/story/src/client/App.tsx
@@ -1,5 +1,11 @@
+import Test from "./Test";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Test a="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo aliquid, id consequatur nemo, nam recusandae quas minima distinctio reiciendis doloribus rerum veniam ad aperiam optio fugit ratione quibusdam vero exercitationem." />
+    </div>
+  );
 };
 
 export default App;
